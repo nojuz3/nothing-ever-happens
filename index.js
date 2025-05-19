f3f30f5464ebffcf90bf0d2e04de9c60f6f65dc8
nojuz3 <xnojukaszc@gmail.com>
2025-05-19 18:51:48 +0300

    nothinghappens

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 
-// https://www.omdbapi.com/?i=tt3896198&apikey=abc56403
-// https://www.omdbapi.com/apikey.aspx?VERIFYKEY=a8c7b43a-51f7-42a2-b27f-82e03403e65f
 const input = document.getElementById("input");
 const submit = document.getElementById("submit");
 const container = document.querySelector(".container")
@@ -17,7 +15,7 @@ submit.addEventListener("click", async (e) => {
     if(inputvalue === ""){
         container.innerHTML = "";
         const name = document.createElement("h2");
-        name.textContent = "Please enter a movie name";
+        name.textContent = "Please enter a superhero name";
 
         const divas = document.createElement("div");
         divas.append(name)
@@ -26,15 +24,17 @@ submit.addEventListener("click", async (e) => {
 
     }else{
         try{
-            const result = await fetch(`http://www.omdbapi.com/?apikey=abc56403&t=${input.value}`);
+            const result = await fetch(`https://www.superheroapi.com/api.php/4dfcedf7b71cb4f4351d31ac5dc62052/search/${inputvalue}`);
             const data = await result.json();
             console.log(data);
+            let list = data.results;
+            console.log(list);
     
 
             container.innerHTML = "";
            
     
-                if(data.Response == "False"){
+                if(data.response == "False" || data.response == "error"){
                     const name = document.createElement("h2");
                     name.textContent = "Does not exist";
         
@@ -43,19 +43,25 @@ submit.addEventListener("click", async (e) => {
         
                     container.append(divas);
                 }else{
-                    const name = document.createElement("h2");
-                    name.textContent = data.Title;
-        
-                    const date = document.createElement("h3");
-                    date.textContent = data.Released;
-        
-                    const img = document.createElement("img");
-                    img.src = data.Poster;
-        
-                    const divas = document.createElement("div");
-                    divas.append(name,date,img)
-        
-                    container.append(divas);
+                    list.forEach(e => {
+                        const name = document.createElement("h2");
+                        name.textContent = e.name;
+                        
+                        const stats = document.createElement("h3");
+                        for (const key in e.powerstats) {
+                            stats.textContent += `${key}: ${e.powerstats[key]} `;
+                        }
+            
+
+                        const img = document.createElement("img");
+                        img.src = e.image.url;
+            
+                        const divas = document.createElement("div");
+                        divas.append(name,stats,img)
+            
+                        container.append(divas);
+                    });
+
                 };
     
         }catch(err){
